4b9ee78e199a0c8bf10f63b7378da65449b2344b
RuslanGavrilov <93270975+RuslanG4@users.noreply.github.com>
2025-04-05 21:16:54 +0100

    #3.4 MAU calculation and visualisation

--- app.py
+++ app.py
@@ -21,5 +21,23 @@ def DailyUniquePlayers(filepath='part3/player_logged_in.csv'):
 
     daily_unique = login_events.groupby('Date')['pid'].nunique()
 
+    daily_unique.index = pd.to_datetime(daily_unique.index)
+
     print(daily_unique)
     return daily_unique
+
+def MonthlyUniquePlayers(filepath='part3/player_logged_in.csv'):
+    import pandas as pd
+
+    df = pd.read_csv(filepath)
+
+    df['Time'] = pd.to_datetime(df['Time'])
+
+    df['Month'] = df['Time'].dt.to_period('M')
+
+    login_events = df[df['EventName'] == 'player_logged_in']
+
+    monthly_unique = login_events.groupby('Month')['pid'].nunique()
+
+    print(monthly_unique)
+    return monthly_unique
