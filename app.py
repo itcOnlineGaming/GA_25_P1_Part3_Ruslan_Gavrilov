7aa053a785ce3c87e8603fd6ba48f846b4500577
RuslanGavrilov <93270975+RuslanG4@users.noreply.github.com>
2025-04-05 21:07:58 +0100

    #3.3 DAU calculation and visualisation

--- app.py
+++ app.py
@@ -9,3 +9,17 @@ def CountUniquePlayers(filepath='part3/player_logged_in.csv'):
     
     print(f"Total unique players who logged in: {unique_players}")
     return unique_players
+
+def DailyUniquePlayers(filepath='part3/player_logged_in.csv'):
+    df = pd.read_csv(filepath)
+
+    df['Time'] = pd.to_datetime(df['Time'])
+
+    df['Date'] = df['Time'].dt.date
+
+    login_events = df[df['EventName'] == 'player_logged_in']
+
+    daily_unique = login_events.groupby('Date')['pid'].nunique()
+
+    print(daily_unique)
+    return daily_unique
